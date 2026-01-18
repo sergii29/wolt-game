3355a142447cb358f6533334afc03d8f6b295628
sergii29 <shalfeyy@gmail.com>
2026-01-18 18:57:52 +0100

    database.js

---
+++ database.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyChrQMyO2soPgoEHq_f3aYs5Cs19q3sWEk",
+  authDomain: "warszawa-courier.firebaseapp.com",
+  databaseURL: "https://warszawa-courier-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "warszawa-courier",
+  storageBucket: "warszawa-courier.firebasestorage.app",
+  messagingSenderId: "295860613508",
+  appId: "1:295860613508:web:86fe8364377d6875612dd1",
+  measurementId: "G-TGSCHBVLX2"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+window.db = db;
+console.log("Firebase initialized for Wolt");
