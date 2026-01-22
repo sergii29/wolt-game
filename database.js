13ff32d69d6623d76212046eb7af47daf85490d6
sergii29 <shalfeyy@gmail.com>
2026-01-23 00:08:55 +0100

    database.js

--- database.js
+++ database.js
@@ -1,28 +1,23 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyChrQMyO2soPgoEHq_f3aYs5Cs19q3sWEk",
-    authDomain: "warszawa-courier.firebaseapp.com",
-    databaseURL: "https://warszawa-courier-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "warszawa-courier",
-    storageBucket: "warszawa-courier.firebasestorage.app",
-    messagingSenderId: "295860613508",
-    appId: "1:295860613508:web:86fe8364377d6875612dd1",
-    measurementId: "G-TGSCHBVLX2"
+  apiKey: "AIzaSyChrQMyO2soPgoEHq_f3aYs5Cs19q3sWEk",
+  authDomain: "warszawa-courier.firebaseapp.com",
+  databaseURL: "https://warszawa-courier-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "warszawa-courier",
+  storageBucket: "warszawa-courier.firebasestorage.app",
+  messagingSenderId: "295860613508",
+  appId: "1:295860613508:web:86fe8364377d6875612dd1",
+  measurementId: "G-TGSCHBVLX2"
 };
 
 firebase.initializeApp(firebaseConfig);
 window.db = firebase.database();
 
-// Изначальные данные игрока (добавлены все 7 параметров)
-window.gameState = JSON.parse(localStorage.getItem("WARSZAWA_FOREVER")) || {
+// Глобальный объект состояния (фиксируем структуру)
+const SAVE_KEY = "WARSZAWA_FOREVER";
+window.state = JSON.parse(localStorage.getItem(SAVE_KEY)) || {
     balance: 100,
-    energy: 100,
-    water: 100,
-    bike: 100,
-    bag: 100,
-    phone: 100,
-    clothes: 100,
-    mood: 100,
-    isEnergyDrinkActive: false
+    items: { bike: 100, bag: 100, phone: 100, gear: 100 }, // Вел, Сумка, Связь, Одежда
+    needs: { energy: 100, water: 100, mood: 100 }         // Силы, Вода, Настроение
 };
 
-console.log("Database & Firebase Ready");
+console.log(">>> Database Ready. Key: WARSZAWA_FOREVER");
